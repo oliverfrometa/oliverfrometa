7fc12a7a5763962932cdc6b28ac9089fb46de057
Oliver Frometa <oliverfrometa17@gmail.com>
2022-09-13 21:31:14 -0400

    new build

--- js/script.js
+++ js/script.js
@@ -1,11 +1,3 @@
-const activePage = window.location.pathname;
-const navLinka = document.querySelectorAll("nav a").forEach(link => {
-    if(link.href.includes(`${activePage}`)) {
-        link.classList.add("active")
-    }
-})
-
-
 const scrollGallery = document.querySelector(".scroll-gallery");
 
 scrollGallery.addEventListener("wheel", (evt) => {
