730e5e5d0b81fa622b5e05566dbe68c37fdc3d71
Oliver Frometa <63164558+oliverfrometa@users.noreply.github.com>
2023-05-30 18:20:48 -0400

    v2

--- js/main.js
+++ js/main.js
@@ -3,8 +3,6 @@ let quotes = [
     "Focus on things you can control. Let go of things you can't.",
     "Your life is as good as your mindset.",
     "Act like the person you want to become."
-
-    // '<img src="imgs/1.jpg"><img/>'
 ];
 
 let newQuoteSection = document.querySelector("#newQuoteSection");
